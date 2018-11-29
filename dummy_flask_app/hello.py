faa37b5200c98be741e8d3609284d684793d4033
Gillian O Connor <gillian.oconnor2@mydit.ie>
2018-11-29 18:52:55 +0000

    add an about view

--- dummy_flask_app/hello.py
+++ dummy_flask_app/hello.py
@@ -1,15 +1,10 @@
-import requests
-from  flask import Flask, render_template, request
-
+from flask import Flask, render_template
 app = Flask(__name__)
-
 @app.route('/')
 def home():
     return render_template('home.html')
 @app.route('/about/')
 def about():
     return render_template('about.html')
-
-
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(debug=True)
